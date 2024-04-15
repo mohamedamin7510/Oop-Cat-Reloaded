19b2ea4de8e08e9151bffaa7a294685ac50a18ad
mohamed amin <maming7510@gmail.com>
2024-04-15 21:26:23 +0200

    Add files via upload
    first commit

---
+++ Task2_oop/Task2_oop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task2_oop.RectangleClass;
+namespace Task2_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var F_rectangle = new Rectangle(5,4);
+            print(F_rectangle);
+
+            var s_rectangle = new Rectangle(10.2,10.3);
+            print(s_rectangle);
+
+            var th_rectangle = new Rectangle(6,9);
+            print(th_rectangle);
+        }
+
+
+        /// <summary>
+        /// this method for printing perimeter and area for rectangle
+        /// </summary>
+        /// <param name="x"></param>
+        public static void print(Rectangle x) 
+        {
+            Console.WriteLine($"the perimeter is equal: {x.Perimeter()} \n" +
+                  $"the Area is equal: {x.Area()}");
+            Console.WriteLine("*-----------*");
+        }
+    }
+}
---
+++ Task2_oop/Task2_oop/RectangleClass/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_oop.RectangleClass
+{
+    internal class Rectangle
+    {
+        private double Length { get; set; }
+        private double Width { get; set; }
+        public Rectangle(double length,double width)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+        public double Area()
+        {
+            var result = Length * Width;
+            return result;
+        }
+        public double Perimeter() 
+        {
+            var result= (Length+Width)*2;
+            return result;
+        }
+    }
+}
